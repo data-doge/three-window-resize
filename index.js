8356a4523dcc520ec8d0f2c2d4b8cd3d6089fb40
euglazer <eugenestapler@gmail.com>
2016-02-13 01:47:54 -0800

    formatting

--- index.js
+++ index.js
@@ -5,27 +5,34 @@ module.exports = function (renderer, camera, dimension) {
    * @author SebastianNette / https://github.com/SebastianNette
    */
 
-	dimension = dimension || function () { return { width: window.innerWidth, height: window.innerHeight } }
+	dimension = dimension || function () {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+  }
 
 	var callback	= function(){
 		// fetch target renderer size
 		var rendererSize = dimension();
+
 		// notify the renderer of the size change
-		renderer.setSize( rendererSize.width, rendererSize.height )
+		renderer.setSize(rendererSize.width, rendererSize.height)
+
 		// update the camera
 		camera.aspect	= rendererSize.width / rendererSize.height
 		camera.updateProjectionMatrix()
 	}
+
 	// bind the resize event
 	window.addEventListener('resize', callback, false)
+
 	// return .stop() the function to stop watching window resize
 	return {
 		trigger	: function(){
 			callback()
 		},
-		/**
-		 * Stop watching window resize
-		*/
+  // Stop watching window resize
 		destroy	: function(){
 			window.removeEventListener('resize', callback)
 		}
