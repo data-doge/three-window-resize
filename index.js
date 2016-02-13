cc5e8c6569c69e28696aefa5504b5478229723af
euglazer <eugenestapler@gmail.com>
2016-02-13 01:30:50 -0800

    repackage THREEx.WindowResize as a node module

---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = function (THREE) {
+
+  /**
+	 * @author jeromeetienne / https://github.com/jeromeetienne
+	 * @author SebastianNette / https://github.com/SebastianNette
+	 */
+
+  THREEx.WindowResize	= function(renderer, camera, dimension){
+  	dimension 	= dimension || function(){ return { width: window.innerWidth, height: window.innerHeight } }
+  	var callback	= function(){
+  		// fetch target renderer size
+  		var rendererSize = dimension();
+  		// notify the renderer of the size change
+  		renderer.setSize( rendererSize.width, rendererSize.height )
+  		// update the camera
+  		camera.aspect	= rendererSize.width / rendererSize.height
+  		camera.updateProjectionMatrix()
+  	}
+  	// bind the resize event
+  	window.addEventListener('resize', callback, false)
+  	// return .stop() the function to stop watching window resize
+  	return {
+  		trigger	: function(){
+  			callback()
+  		},
+  		/**
+  		 * Stop watching window resize
+  		*/
+  		destroy	: function(){
+  			window.removeEventListener('resize', callback)
+  		}
+  	}
+  }
+
+}
